e7bfdc68a6a6a6079d2c6d5540fa3762a4b25051
Robert <robertcjohnson1984@gmail.com>
2026-02-07 18:26:55 -0700

    fix(sanity): derive image source type from builder

--- lib/sanity/image.ts
+++ lib/sanity/image.ts
@@ -1,9 +1,9 @@
 import imageUrlBuilder from "@sanity/image-url";
-import type { SanityImageSource } from "@sanity/image-url";
 import { client } from "./client";
 
 const builder = imageUrlBuilder(client);
+type ImageSource = Parameters<typeof builder.image>[0];
 
-export function urlFor(source: SanityImageSource) {
+export function urlFor(source: ImageSource) {
   return builder.image(source);
 }
